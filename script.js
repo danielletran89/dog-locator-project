497a7460b1fafc1880d6d1b3640e7c3462e83a30
Danielle Tran <danielletran89@gmail.com>
2021-06-19 11:49:58 -0700

    minor updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 //code for new Token
 //LET variable for Token
 //"IF countdown statement" that will request and replace Token every 50min; (let i = 1min, Token Let Variable = 50min countdown(--), if i > Token, then request new Token)
+
